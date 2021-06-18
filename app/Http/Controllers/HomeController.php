7a3e3db35f4f6c7f8663e178ef1c29f4c9bc9373
danialessi <daniele.alessi@hotmail.it>
2021-06-18 17:53:30 +0200

    stampati post nella home pubblica

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('guest.home');
+        $posts = Post::all();
+
+        $data = [
+            'posts' => $posts
+        ];
+
+
+        return view('guest.home', $data);
     }
 }
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,6 +3,21 @@
 @section('content')
     <div class="container">
         <h1>Homepage pubblica</h1>
+
+        <h2>Elenco post</h2>
+
+        <div class="row">
+            @foreach($posts as $post)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">{{$post->title}}</h5>
+                  <p class="card-text">{{$post->content}}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        
     </div>
     
 @endsection
